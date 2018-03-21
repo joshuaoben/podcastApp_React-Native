501baa72344c54a914c9122af98ddc4b2665c7fb
joshuaoben <olonadejoshua@gmail.com>
2018-03-21 22:26:08 +0100

    fetching data dynamically using local API

--- src/components/BoxList.js
+++ src/components/BoxList.js
@@ -1,41 +1,39 @@
 import React,  {Component} from 'react';
 import {Text, View, Image} from 'react-native';
+import pods from '../data.json';
 import Box from './Box';
-import axios from'axios';
+
 
 class BoxList extends Component{
-	const {boxListStyle} = styles;
 
-	state = {
-		pods: []
-	};
+	state = { pods: [] };
 
 	componentWillMount(){
-		axios.get('../data.json').then(response => this.setState({pods: response.data }));
+		this.setState({pods: pods });
 	}
 
-	renderBoxList
-
-
+	renderBoxList(){
+		return this.state.pods.map(pod =>
+			<Box number={pod.number} subtitle={pod.subtitle} title={pod.title} />
+			);
+	}
 
 	render(){
 
+	const styles = {
+		boxListStyle: {
+			backgroundColor: 'rgba(0,0,0,0.1)',
+			padding: 10,
+			width: '100%'
+		}
+	}
+
 	return(
-			<View style={boxListStyle}>
-				<Box number={1} subtitle={'Why You should Learn'} title={'Web Development as a problem solver'} />
-				<Box number={2} subtitle={'Fun tips about'} title={'Living as a Programmer'} />
-				<Box number={3} subtitle={'Pros and Cons of'} title={'Learning Programming by yourself'} />
+			<View style={styles.boxListStyle}>
+				{ this.renderBoxList() }
 			</View>
-		)
-}
-}
-const styles = {
-	boxListStyle: {
-		backgroundColor: 'rgba(0,0,0,0.1)',
-		padding: 10,
-		width: '100%'
+		);
 	}
 }
-
 
 export default BoxList;
